88b05ae234af078da7954cd6cb96dd3093371692
Andrea Malleo <akmalleo3@gmail.com>
2021-05-13 23:40:26 -0400

    parallel game play?

--- train.py
+++ train.py
@@ -7,27 +7,79 @@ import torch
 from torch import optim
 from torch import nn
 
+import evaluate_net as en
+
 batchSize =  512
 width = 5
 height = 5
+'''
 def collectGameData(network, useNetwork,T):
     #play games and then sample uniformly from the aggregated data for training
     game_images = []
     game_targets = []
-    for _ in range(50):
+    for _ in range(40):
         game = ConnectFour(width,height,True)
         images, targets = play_game(game, network, useNetwork,T)
         game_images.append(images)
         game_targets.append(targets)
     flattened_images = list(itertools.chain.from_iterable(game_images))
     flattened_targets = list(itertools.chain.from_iterable(game_targets))
-    batchSize = min(len(flattened_images), 512)
+    batchSize = min(len(flattened_images), 200)
     sample_indices = numpy.random.choice(range(len(flattened_images)),batchSize)
     sample_images = [flattened_images[i] for i in sample_indices]
     sample_targets = [flattened_targets[i] for i in sample_indices]
 
     return sample_images, sample_targets
-    #return list(zip(sample_images, sample_targets))
+'''
+
+from torch.multiprocessing import Process, set_start_method, Queue, Barrier
+try:
+     set_start_method('spawn')
+except RuntimeError:
+    pass
+
+def collectGameData(b, play, network, useNetwork, T, width, height, image_q, target_q):
+    images, targets = play(network,useNetwork, T, width, height)
+    image_q.put(images)
+    target_q.put(targets)
+    b.wait()
+
+import copy
+#play games and then sample uniformly from the aggregated data for training        
+def collectGameDataParallel(network, useNetwork,T, width, height):
+    images = Queue()
+    targets = Queue()
+    ngames = 3
+    barrier = Barrier(ngames +1)
+
+    processes=[Process(target=collectGameData, args=(barrier,play_game, network,\
+                         useNetwork, T, width,height, images, targets)) \
+                            for _ in range(ngames)]
+    for p in processes:
+        p.start()
+
+    game_images = []
+    game_targets = []
+    for _ in range(ngames):
+        im = images.get()
+        game_images.append(copy.deepcopy(im))
+        del im
+        t = targets.get()
+        game_targets.append(copy.deepcopy(t))
+        del t
+    barrier.wait()
+
+    for p in processes:
+        p.join()
+
+    flattened_images = list(itertools.chain.from_iterable(game_images))
+    flattened_targets = list(itertools.chain.from_iterable(game_targets))
+    batchSize = min(len(flattened_images), 200)
+    sample_indices = numpy.random.choice(range(len(flattened_images)),batchSize)
+    sample_images = [flattened_images[i] for i in sample_indices]
+    sample_targets = [flattened_targets[i] for i in sample_indices]
+
+    return sample_images, sample_targets
 
 def train(states,targets,model, optimizer,T):
     cuda = torch.device('cuda')
@@ -53,32 +105,43 @@ def train(states,targets,model, optimizer,T):
    
     return model, optimizer
 
+
+def validate(net, T, width):
+    print(f"Validating against random player")
+    win, draw = en.eval_network(en.eval_network_v_random,net,T,width)
+
 def run():
-    T = 4
+    T = 1
     device ='cuda'
     model = PolicyValueNet(width, height,2*T)
-    model.load_state_dict(torch.load("test_longhaul2.pth",map_location=device))
+    #model.load_state_dict(torch.load("test_longhaul3.pth",map_location=device))
 
     #optimizer = optim.SGD(model.parameters(), lr=2e-2, momentum=0.9,weight_decay=1e-4)
     #optimizer = optim.SGD(model.parameters(), lr=2e-1 )
     #scheduler = optim.lr_scheduler.MultiStepLR(optimizer,[20,60])
     model.cuda()
-    optimizer = optim.Adagrad(model.parameters())
+    model.share_memory()
+    optimizer = optim.Adagrad(model.parameters(), lr=1e-3)
 
     batchidx = 0
     useNetwork = False
 
-    while batchidx < 50:
+    while batchidx < 1:
+        if batchidx == 200:
+            useNetwork = True
         model.eval()
         print("Simulating  games")
-        states, targets = collectGameData(model, useNetwork,T)
+        states, targets = collectGameDataParallel(model, useNetwork,T, width, height)
         print("Training")
         model.train()
         model,optimizer = train(states,targets,model,optimizer,T)
+        if batchidx % 20:
+            validate(model, T, width)
         #scheduler.step()
         #print(f"Learning rate: {scheduler.get_last_lr()}")
         print(f"Saving batch {batchidx}")
-        torch.save(model.state_dict(), "test_longhaul2.pth")
+        torch.save(model.state_dict(), "testing.pth")
         batchidx +=1
 
-run()
+if __name__ == '__main__':
+    run()
